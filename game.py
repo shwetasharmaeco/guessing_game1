a170ac95d16793be68d015573b30e1b05c75a221
Shweta Sharma <shwetasharmaeco@gmail.com>
2020-04-28 23:40:06 +0000

    optin to replay included

--- game.py
+++ game.py
@@ -7,14 +7,20 @@ print ("I am thinking a number between 1 to 100 \n Can you guess it?")
 
 
 comp_num= randint(1,101)
-count=0
+count=1
 while True:
    
     try:
         guess= int(input("What is your guess > "))
         if guess==comp_num:
             print(f"Well done {name}! You found my number in {count} tries")
-            break
+            restart=input("would you like to play again: Y or N> ").upper()	
+            if  restart=="Y":
+            	comp_num= randint(1,101)
+            	continue
+            else:
+            	print("Thanks for playing, see you next time")
+            	break
         elif guess<1 or guess>100:
         	print("Only numbers between 1-100 allowed")
         elif guess < comp_num:
