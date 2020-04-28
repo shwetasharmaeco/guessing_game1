387043874584727053b34d70128e5d58c6b2fd1e
Shweta Sharma <shwetasharmaeco@gmail.com>
2020-04-28 22:41:00 +0000

    error bug fixed

--- game.py
+++ game.py
@@ -7,19 +7,22 @@ print ("I am thinking a number between 1 to 100 \n Can you guess it?")
 
 
 comp_num= randint(1,101)
-print(comp_num)
-
 count=0
 while True:
-	guess= int(input("What is your guess > "))
-	if guess==comp_num:
-		print(f"Well done {name}! You found my number in {count} tries")
-		break
-	elif guess < comp_num:
-		print("Your guess is too low, try again.")
-		count+=1
-	elif guess > comp_num:
-		print("Your guess is too high, try again.")
-		count+=1
-	else:
-		print ("Invalid command/number")
+   
+    try:
+        guess= int(input("What is your guess > "))
+        if guess==comp_num:
+            print(f"Well done {name}! You found my number in {count} tries")
+            break
+        elif guess < comp_num:
+            print("Your guess is too low, try again.")
+            count+=1
+        elif guess > comp_num:
+            print("Your guess is too high, try again.")
+            count+=1
+        
+    except:
+        print("Only numbers between 1-100 allowed")
+
+	
